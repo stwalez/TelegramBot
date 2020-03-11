6b82723320989d5536a9b84f48c2c6c8358bceae
Olawale <stwalez1@gmail.com>
2020-03-11 09:34:15 +0100

    removed unnecessary lines

--- app.js
+++ app.js
@@ -239,46 +239,6 @@ bot.onText(/\/start/, (msg) => {
     );
 });
 
-
-// Matches /editable
-bot.onText(/\/editable/, function onEditableText(msg) {
-    console.log(msg)
-    const opts = {
-      reply_markup: {
-        inline_keyboard: [
-          [
-            {
-              text: 'Edit Text',
-              // we shall check for this value when we listen
-              // for "callback_query"
-              callback_data: 'edit'
-            }
-          ]
-        ]
-      }
-    };
-    bot.sendMessage(msg.from.id, 'Original Text', opts);
-  });
-  
-  
-  // Handle callback queries
-  bot.on('callback_query', function onCallbackQuery(callbackQuery) {
-    const action = callbackQuery.data;
-    const msg = callbackQuery.message;
-    const opts = {
-      chat_id: msg.chat.id,
-      message_id: msg.message_id,
-    };
-    let text;
-  
-    if (action === 'edit') {
-      text = 'Edited Text';
-    }
-  
-    bot.editMessageText(text, opts);
-  });
-
-
   // Including a Movie API 
 bot.onText(/\/movie (.+)/, (msg, match) => {
     var movie = match[1];
